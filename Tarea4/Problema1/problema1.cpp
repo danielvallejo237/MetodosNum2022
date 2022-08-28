fc40f6d9a7d7c1521d2cfeee9c49d06f702db17e
danielvallejo237 <daniel.vallejo@cimat.mx>
2022-08-27 21:26:14 -0500

    tarea4

---
+++ Tarea4/Problema1/problema1.cpp
@@ -0,0 +1,156 @@
+#include<bits/stdc++.h>
+#include<cmath> 
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<ctime>
+#include<chrono>
+using namespace std;
+
+//Incluimos las mismas librerías que en el problema anterior
+
+class Matrix 
+{
+    public:
+    int n,m; //Dimensiones de la matriz
+    vector<double> matrix;
+    int type;
+    Matrix(vector<double> &mat,int n, int m)
+    {
+        this->matrix=mat;
+        this->n=n;
+        this->m=m;
+    }
+    Matrix(string filename)
+    {
+        //Podemos inicializar ahora una matriz con el nombre del archivo
+        ifstream ifs(filename);
+        string line;
+        getline(ifs, line);
+        istringstream iss(line);
+        int n,m;
+        iss>>n>>m;
+        this->n=n;
+        this->m=m;
+        double a;
+        while (ifs >> a) this->matrix.push_back(a);
+    }
+    Matrix(){}
+    ~Matrix(){}
+    void set_dimensions(int n, int m)
+    {
+        this->n=n;
+        this->m=m;
+    }
+    void set_vector(vector<double> &vec)
+    {
+        this->matrix=vec;
+    }
+    void set_type(int type)
+    {
+        this->type=type;
+    }
+    void print_content()
+    {
+        for(int i=0;i<this->n;i++)
+        {
+            for(int j=0;j<this->m;j++)
+            {
+                cout<<get(i,j)<<"\t";
+            }
+            cout<<endl;
+        }
+    }
+    void print_to_text(string filename)
+    {
+        ofstream os{filename};
+        os<<this->n<<" "<<this->m<<endl;
+        for(int i=0;i<this->n;i++)
+        {
+            for(int j=0;j<this->m;j++) os<<this->matrix[i*this->m+j]<<" ";
+            os<<endl;
+        }
+    }
+    void swap_row(int a, int b)
+    {
+        //Funcion para cambiar los renglones de la matriz en caso de pivoteo
+        for(int i=0;i<this->m;i++) swap(matrix[this->m*a+i],this->matrix[this->m*b+i]);
+    }
+    Matrix Transpose()
+    {
+        vector<double> T(this->n*this->m);
+        for(int i=0;i<this->n;i++)for(int j=0;j<this->m;j++) T[j*this->n+i]=this->matrix[i*this->m+j];
+        Matrix M(T,this->m,this->n);
+        return M; 
+    }
+    double get(int row,int col)
+    {
+        return this->matrix[row*this->m+col];
+    }
+    void put(int row, int col, double value)
+    {
+        this->matrix[row*this->m+col]=value; 
+    }
+    pair<int,int> shape()
+    {
+        return make_pair(this->n,this->m); 
+    }
+    //Sobrecarga de operadores para sumar matrices en lugar de sumarlos de una sola vez
+    Matrix operator + (Matrix const &obj)
+    {
+        if(this->n==obj.n && this->m==obj.m)
+        {
+            //Tienen que tener las mismas dimensiones
+            for(int i=0;i<this->n*this->m;i++) this->matrix[i]+=obj.matrix[i];
+        }
+        return *this;
+    }
+    Matrix operator - (Matrix const &obj)
+    {
+        if(this->n==obj.n && this->m==obj.m)
+        {
+            //Tienen que tener las mismas dimensiones
+            for(int i=0;i<this->n*this->m;i++) this->matrix[i]-=obj.matrix[i];
+        }
+        return *this;
+    }
+    double norm()
+    {
+        double norma;
+        if(this->n>1 && this->m>1)
+        {
+            norma=0;
+            for(int i=0;i<this->n;i++)
+            {
+                double aux=0;
+                for(int j=0;j<this->m;j++) aux+=fabs(get(i,j));
+                if (aux>norma) norma=aux;
+            }
+        }
+        else if (this->m==1 && this->n>1)
+        {
+            norma=0;
+            for(int i=0;i<this->n;i++) norma+=get(i,0)*get(i,0);
+            norma=sqrt(norma);
+        }
+        else if(this->m>1 && this->n==1)
+        {
+            norma=0;
+            for(int i=0;i<this->m;i++) norma+=get(0,i)*get(0,i);
+            norma=sqrt(norma);
+        }
+        else
+        {
+            norma=0;
+        }
+        return norma;
+    }
+};
+
+
+void Cholesky(Matrix A){} //Implementación del método de cholesky
+
+int main(int argv, char* argc[])
+{
+    return 0;
+}
